ba39aec6b5c81bbaeab5c62bf6cbd4ecc4ff37ab
yoyuse <yoyuse@gmail.com>
2025-01-29 18:00:00 +0900

    Fix: add `--` to delimit end of options

--- functions/__fish_ttt_do_ttt.fish
+++ functions/__fish_ttt_do_ttt.fish
@@ -1,9 +1,9 @@
 function __fish_ttt_do_ttt
     set -q FISH_TTT_COMMAND
-    or set -l FISH_TTT_COMMAND cli-ttt
-    set -l rbuffer (string sub -s (math (commandline -C) + 1) (commandline))
+    or set -l FISH_TTT_COMMAND cli-ttt --
+    set -l rbuffer (string sub -s (math (commandline -C) + 1) -- (commandline))
     set -l lbuffer (commandline -c | $FISH_TTT_COMMAND)
-    set -l cursor (string length "$lbuffer")
+    set -l cursor (string length -- "$lbuffer")
     commandline -- "$lbuffer$rbuffer"
     commandline -C $cursor
     commandline -f repaint
