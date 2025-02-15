ef54c5f4f2818bf439e59fd2240508d4edd7ecda
yoyuse <yoyuse@gmail.com>
2025-02-15 20:10:00 +0900

    Fix: for cli-ttt 2025.02.15

--- functions/__fish_ttt_do_ttt.fish
+++ functions/__fish_ttt_do_ttt.fish
@@ -1,6 +1,6 @@
 function __fish_ttt_do_ttt
     set -q FISH_TTT_COMMAND
-    or set -l FISH_TTT_COMMAND cli-ttt -l -q --
+    or set -l FISH_TTT_COMMAND cli-ttt -q --
     set -l rbuffer (string sub -s (math (commandline -C) + 1) -- (commandline))
     set -l lbuffer ($FISH_TTT_COMMAND (commandline -c))
     set -l cursor (string length -- "$lbuffer")
